862b6b878aebf1c5de7467df487ed5702ef020bb
Calum Lim Sheng En <cslim35@student.monash.edu>
2018-09-30 02:28:51 +0800

    half completed menu buttons

---
+++ script/mainpagescript.js
@@ -0,0 +1,6 @@
+function showRightSide() {
+  document.getElementById("rightMenuSVG").style.maxWidth = "0px";
+  document.getElementById("rightMenuSVG").style.minWidth = "0px";
+  document.getElementById("rightMenuSVG").style.right = 0;
+  document.getElementById("rightMenuSVG").style.zIndex = 1;
+}
