c7f8e3c362fc12ba2d3aa418d8d7b3d178e5748d
Calum Lim Sheng En <cslim35@student.monash.edu>
2018-09-30 18:12:47 +0800

    added responsive menu when clicked on option to expands left hand side

--- script/mainpagescript.js
+++ script/mainpagescript.js
@@ -1,6 +1,41 @@
 function showRightSide() {
-  document.getElementById("rightMenuSVG").style.maxWidth = "0px";
-  document.getElementById("rightMenuSVG").style.minWidth = "0px";
-  document.getElementById("rightMenuSVG").style.right = 0;
-  document.getElementById("rightMenuSVG").style.zIndex = 1;
+  var rightMenuSVG = document.getElementById("rightMenuSVG");
+  shrinkLeftMenu();
+  rightMenuSVG.style.zIndex = 1;
+  rightMenuSVG.style.animation = "";
+  rightMenuSVG.style.animation = "slideleft-hover 1s forwards";
+  rightMenuSVG.style.animationDelay = "0.15s";
+}
+
+function shrinkLeftMenu() {
+  var leftMenuSVG = document.getElementById("leftMenuSVG");
+
+  leftMenuSVG.style.left = "0%";
+  leftMenuSVG.style.animation = "";
+  leftMenuSVG.style.animation = "shrink-leftscreen 1s forwards";
+  leftMenuSVG.style.animationDelay = "0.15s";
+
+  var menuTabs = document.getElementById("welcomeTitle");
+  var menu = document.getElementById("menuOptions");
+
+  //menuTabs.style.marginLeft = "50px";
+  menuTabs.style.left = "0%";
+  menuTabs.style.animation = "";
+  menuTabs.style.animation = "shrink-leftmenu 1s forwards";
+  menuTabs.style.animationDelay = "0.15s";
+  changeText("Title", "L I M")
+  changeText("menuIntro", "intro");
+  changeText("menuExperiences", "experiences");
+  changeText("menuPortfolio", "portfolio");
+  changeText("menuContact", "contact");
+  changeText("menuCredit", "credit");
+
+  menuTabs.style.minWidth = "10%";
+  menuTabs.style.fontSize = "70px";
+}
+
+function changeText(e,i) {
+  element = document.getElementById(e)
+  element.innerHTML = i;
+  element.style.animation = "fadein 1s";
 }
