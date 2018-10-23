b497e5a67442e7c7fbb83462e6c0b18321907a0c
Calum Lim Sheng En <cslim35@student.monash.edu>
2018-10-24 00:19:26 +0800

    intitial design finished

--- polygon/script/welcomescript.js
+++ polygon/script/welcomescript.js
@@ -1,34 +1,14 @@
-var topleft=$('#topleft-triangle');
-var topright=$("#topright-triangle");
-var bottomleft=$('#bottomleft-triangle');
-var bottomright=$("#bottomright-triangle");
-
 var layer=$('html');
 
 layer.mousemove(function(e){
-var valueX=((e.pageX * -1 / 30)+5);
-var valueY=((e.pageY * -1 / 30));
-var valueX2 = ((e.pageX*1/30)-5);
-var valueY2 = ((e.pageY*1/30));
+var valueX=((e.pageX * -1 / 100));
+var valueY=((e.pageY * -1 / 100));
 
-  topleft.css({
-    'transform':'translate3d('+valueX+'px,'+valueY+'px,0)'
-      });
-  topright.css({
-    'transform':'translate3d('+valueX+'px,'+valueY+'px,0)'
-  })
-  bottomleft.css({
-    'transform':'translate3d('+valueX+'px,'+valueY+'px,0)'
-  })
-  bottomright.css({
-    'transform':'translate3d('+valueX+'px,'+valueY+'px,0)'
-  })
+
   $("#name").css({
-    'transform':'translate3d('+valueX2+'px,'+valueY2+'px,0)'
+    'transform':'translate3d('+valueX+'px,'+valueY+'px,0)'
   })
-  $("#name").css({
-    'font-size':+50+valueY2+'px'
+  $("#surname").css({
+    'transform':'translate3d('+valueX+'px,'+valueY+'px,0)'
   })
-
-
 });
